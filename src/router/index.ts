4e88098aa26241672e2e8dbe4694fb94006b9e11
mohlaroyim <mohlaroyimraupova99@gmail.com>
2024-11-15 00:16:17 +0300

    router link test

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import { useAuthStore } from "@/auth/stores/useAuthStore";
 
 const router = createRouter({
-  history: createWebHashHistory('/vue-boilerplate/'),
+  history: createWebHistory('/vue-boilerplate/'),
   routes: [
     {
       path: '/',
