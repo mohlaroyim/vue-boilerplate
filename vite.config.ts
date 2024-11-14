55e86c15117151fc9497446755c501ff8001871a
mohlaroyim <mohlaroyimraupova99@gmail.com>
2024-11-14 13:39:54 +0300

    gh config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vue-boilerplate/' : '/',
   plugins: [
     vue(),
   ],
